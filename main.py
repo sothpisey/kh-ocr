dd174a42b1de0bc569822c844ebbbc1c26d32db8
SOTH Pisey <sothpiseyunique@gmail.com>
2024-11-28 02:18:55 +0700

    Rename imported clase in order to follow naming conventions in Python.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from capture import canvas
+from capture import Canvas
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = canvas()
+    window = Canvas()
     window.show()
     sys.exit(app.exec())
